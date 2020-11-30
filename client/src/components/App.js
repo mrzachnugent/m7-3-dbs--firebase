a1a434649a20cb2bdb61f930925d3e6ed8cf2538
Zachary Nugent <63797719+MixReferences@users.noreply.github.com>
2020-11-30 13:29:16 -0500

    complete the workshop M7-3 firebaseAuth/realtime database

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,28 @@
-import React, { useContext, useEffect } from 'react';
-import styled from 'styled-components';
-import Avatar from './Avatar';
+import React, { useContext, useEffect } from "react";
+import styled from "styled-components";
+import Avatar from "./Avatar";
 
-import { AppContext } from './AppContext';
+import { AppContext } from "./AppContext";
 
 const App = () => {
-  const {} = useContext(AppContext);
+  const { appUser, signInWithGoogle, handleSignOut, message } = useContext(
+    AppContext
+  );
 
   return (
     <StyledPageWrapper>
       <StyledHeader>
-        <button>Sign In</button>
+        {appUser && appUser.email ? (
+          <StyledUserContainer>
+            <Avatar src={appUser.photoURL} />
+            <p>
+              {appUser.displayName} ({appUser.email})
+              <button onClick={handleSignOut}>Sign out</button>
+            </p>
+          </StyledUserContainer>
+        ) : (
+          <button onClick={signInWithGoogle}>Sign In</button>
+        )}
       </StyledHeader>
       <StyledContainer>{message}</StyledContainer>
     </StyledPageWrapper>
--- client/src/components/AppContext.js
+++ client/src/components/AppContext.js
@@ -1,9 +1,58 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext } from "react";
+import apiKey from "../keys";
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase";
+import "firebase/auth";
+
+const firebaseApp = firebase.initializeApp(apiKey);
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
 
 export const AppContext = createContext(null);
 
-const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+const AppProvider = ({ children, signInWithGoogle, signOut, user }) => {
+  const [appUser, setAppUser] = React.useState({});
+  const [message, setMessage] = React.useState("");
+
+  React.useEffect(() => {
+    if (user) {
+      fetch(`/users`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          displayName: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+        }),
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          setAppUser(json.data);
+          setMessage(json.message);
+        });
+    }
+  }, [user]);
+
+  const handleSignOut = () => {
+    signOut();
+    setAppUser({});
+  };
+
+  return (
+    <AppContext.Provider
+      value={{ appUser, signInWithGoogle, handleSignOut, message }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
-export default AppProvider;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(AppProvider);
